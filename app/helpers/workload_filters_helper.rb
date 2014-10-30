08d62ed234eaf9c70a3f5a7defb88e8506a8f94f
Todd Hambley <thambley@travelleaders.com>
2014-10-30 16:46:59 -0400

    #4494 simplify and sort user list

--- app/helpers/workload_filters_helper.rb
+++ app/helpers/workload_filters_helper.rb
@@ -3,11 +3,13 @@ module WorkloadFiltersHelper
   def get_option_tags_for_userselection(usersToShow, selectedUsers)
 
     result = '';
+    
+    usersToShow.sort_by!{ |u| u.name }
 
     usersToShow.each do |user|
       selected = selectedUsers.include?(user) ? 'selected="selected"' : ''
 
-      result += "<option value=\"#{h(user.id)}\" #{selected}>#{h(user.firstname)} #{h(user.lastname)} (#{h(user.name)})</option>"
+      result += "<option value=\"#{h(user.id)}\" #{selected}>#{h(user.name)}</option>"
     end
 
     return result.html_safe
